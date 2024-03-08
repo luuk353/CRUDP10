3f29ecf5d6e1d391a9a3bf1dc339aa45286fcfdd
Naser Jafari <naserjafari5002@gmail.com>
2024-03-08 16:02:51 +0100

    background gradient toegevoegd aan forum

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <main>
-    <div class="h-screen bg-gray-100">
+    <div class="h-screen bg-gray-100 bg-gradient-to-l from-green-600 via-amber-300 to-red-600">
         <section class="flex justify-center items-center p-6 h-full">
             <div class="bg-white w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 p-6 rounded-lg shadow-md">
                 <h1 class="text-3xl font-bold mb-4">Forum</h1>
@@ -25,15 +25,18 @@
                 <!-- Forum Posts -->
                 <div>
                     <h2 class="text-xl font-semibold mb-4">Forum Posts</h2>
-                    var_dump($forum);
-                    @foreach ($forum as $post)
-                        <div class="bg-gray-200 p-4 mb-4 rounded-lg">
-                            <h3 class="text-lg font-bold mb-2">{{ $post->title }}</h3>
-                            <p class="text-gray-800">{{ $post->text }}</p>
-                            <p class="text-gray-800">{{ $post->text }}</p>
-                            <p class="text-gray-800">{{ $post->name }}</p>
-                        </div>
-                    @endforeach
+                    @if($forum->count() < 1)
+                        Er zijn nog geen forum posts!
+                    @else
+                        @foreach ($forum as $post)
+                            <div class="bg-gray-200 p-4 mb-4 rounded-lg">
+                                <h3 class="text-lg font-bold mb-2">{{ $post->title }}</h3>
+                                <p class="text-gray-800">{{ $post->text }}</p>
+                                <p class="text-gray-800">{{ $post->text }}</p>
+                                <p class="text-gray-800">{{ $post->name }}</p>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </section>
