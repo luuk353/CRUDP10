1d4c955d4a98caab59beb8be9b18e66911b514fc
Naser Jafari <naserjafari5002@gmail.com>
2024-04-05 09:41:41 +0200

    custom error pages, alleen voor 404 en 500 error. Vite config beetje aangepast en dit keer wel die font goed ingesteld

---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.error_layout')
+
+@section('content')
+    <main class="flex min-h-screen max-w-full items-center justify-center">
+        <h1 class="text-4xl">404 error | Pagina bestaat niet!!!</h1>
+    </main>
+@endsection
