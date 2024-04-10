ca1fe7ea024fbcf7d0d0ca3c88a1ef98cd0a06e6
Naser Jafari <naserjafari5002@gmail.com>
2024-04-10 19:17:41 +0200

    onnodige scss files weggehaald en ander foto + naam

--- resources/views/live-chat/index.blade.php
+++ resources/views/live-chat/index.blade.php
@@ -4,11 +4,18 @@
     <main class="flex min-h-screen justify-center bg-gradient-to-l from-purple-800 via-pink-700 to-red-600 p-12">
         <div class="chat w-full max-w-xl rounded-lg bg-white p-6 shadow-lg">
             <div class="top mb-4 flex items-center">
-                <img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Avatar"
-                    class="mr-4 h-12 w-12 rounded-full">
+                <img src="{{ asset('images/1705499654.png') }}" alt="Avatar" class="mr-4 h-12 w-12 rounded-full">
                 <div>
-                    <p class="text-xl font-bold">Ross Edlin</p>
-                    <small class="text-gray-600">Online</small>
+                    <p class="text-xl font-bold">Quincy Berkleef</p>
+                    <div class="flex space-x-1">
+                        <small class="text-gray-600">Online
+                        </small><svg class="h-6 w-6 text-green-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd"
+                                d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
                 </div>
             </div>
             <div id="messageContainer" class="messages min-h-screen overflow-y-auto">
